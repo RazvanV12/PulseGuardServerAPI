3b64223d9d3e081213a65618c38999b965684411
Velciu A Razvan-Gabriel <92295676+RazvanV12@users.noreply.github.com>
2026-06-05 15:53:19 +0300

    Initial commit

---
+++ src/main/java/com/personal/pulseguardserverapi/PulseGuardServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.pulseguardserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseGuardServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseGuardServerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/pulseguardserverapi/PulseGuardServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.pulseguardserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseGuardServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
